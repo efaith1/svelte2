7f7379cb990c7298c475917cef9be2850911d4a6
Esther Faith <efk@mit.edu>
2024-03-05 03:09:04 -0500

    literal complete lab 4

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,20 +1,3 @@
-<article>
-    <a href="../A2.html">
-        <h2>6.C35 Assignment 2: Exploratory Data Analysis</h2>
-        <img src="../images/g2.webp" alt="">
-    </a>
-    <p>In this assignment, you will begin to work with public and proprietary datasets related to MAPC’s research themes and subthemes. You will choose one subtheme (i.e., one dataset) to conduct exploratory analysis using visualization, with a focus on understanding the shape & structure of the data, investigating initial questions, and developing preliminary insights & hypotheses.</p>
-</article>
-
-<article>
-    <a href="../A3.html">
-        <h2>6.C35 Assignment 3: Visualization Design</h2>
-        <img src="../images/three.png" alt="">
-    </a>
-    <p>In the previous assignment, you created a large number of visualizations but likely stuck with the default design choices the software package (e.g., Tableau) made. In this assignment, you will instead focus on these design choices by sketching 3 visualizations, and then crafting a single visualization from a small dataset. You should, in theory, be able to justify the contribution of every element of the visualization.</p>
-</article>
-
-
 <script>
     export let info = {};
     export let hLevel = 2; 
@@ -30,6 +13,10 @@
     }
 </style>
 
-<svelte:element this={"h" + hLevel}>{p.title}</svelte:element>
-<img src={p.image} alt="">
-<p>{p.description}</p>
+<article>
+<a href={info.link}>
+    <svelte:element this={"h" + hLevel}>{info.title}</svelte:element>
+</a>
+<img src={info.image} alt="">
+<p>{info.description}</p>
+</article>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-import projects from '$lib/projects.json';
-import Project from "$lib/Project.svelte";
+    import projects from '$lib/projects.json';
+    import Project from "$lib/Project.svelte";
 </script>
 
 <h1>{projects.length} Projects </h1>
 <svelte:head>
-	<title>Projects</title>
+    <title>Projects</title>
 </svelte:head>
 
 <div class="projects">
@@ -13,4 +13,3 @@ import Project from "$lib/Project.svelte";
         <Project info={p} />        
     {/each}
 </div>
-
