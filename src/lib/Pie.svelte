bfc7b55da1634789589fd98d690ade75dc9b2830
Esther Faith <efk@mit.edu>
2024-04-19 11:31:06 -0400

    super simple pie

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -3,143 +3,148 @@
 
   export let data = [];
   export let selectedIndex = -1;
-
   export let colors = d3.scaleOrdinal(d3.schemeTableau10);
-
-  let pieData;
-  let wedges = {};
-  let oldData = [];
   export let transitionDuration = 3000; // add ms or nah
 
+  let arcData = [];
+  let pieData;
+  let arcs = [];
+  // let wedges = {}; // step 4
+  // let oldData = []; // step 4
+  let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+  let sliceGenerator = d3
+    .pie()
+    .value((d) => d.value)
+    .sort(null);
+
   $: {
-    pieData = d3.sort(data, (d) => d.label);
-    oldData = pieData;
+    pieData = data.map((d) => ({ ...d }));
+  }
+  $: {
+    // oldData = pieData; // step 4
 
-    let arcData = [];
-    let arcs = [];
-
-    let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-    let sliceGenerator = d3
-      .pie()
-      .value((d) => d.value)
-      .sort(null);
     arcData = sliceGenerator(pieData);
     arcs = arcData.map((d) => arcGenerator(d));
-
+    pieData = d3.sort(data, (d) => d.label);
     pieData = pieData.map((d, i) => ({ ...d, ...arcData[i], arc: arcs[i] }));
-    transitionArcs();
+    // transitionArcs();
   }
 
   // function 1/6 - called below, looks fine
-  function sameArc(arc1, arc2) {
-    if (!arc1 || !arc2) {
-      return !arc1 && !arc2;
-    }
-    return (
-      arc1.startAngle === arc2.startAngle && arc1.endAngle === arc2.endAngle
-    );
-  }
+  // function sameArc(arc1, arc2) {
+  //   if (!arc1 || !arc2) {
+  //     return !arc1 && !arc2;
+  //   }
+  //   return (
+  //     arc1.startAngle === arc2.startAngle && arc1.endAngle === arc2.endAngle
+  //   );
+  // }
 
   // function 2/6 - wrote
-  function transitionArcs() {
-    let wedgeElements = Object.values(wedges);
+  // function transitionArcs() {
+  //   let wedgeElements = Object.values(wedges);
 
-    d3.selectAll(wedgeElements)
-      .transition("arc")
-      .duration(transitionDuration)
-      .styleTween("d", function (_, index) {
-        let wedge = this;
-        let label = Object.keys(wedges)[index];
-        let transition = transitionArc(wedge, label);
-        return transition?.interpolator;
-      });
-  }
+  //   d3.selectAll(wedgeElements)
+  //     .transition("arc")
+  //     .duration(transitionDuration)
+  //     .styleTween("d", function (_, index) {
+  //       let wedge = this;
+  //       let label = Object.keys(wedges)[index];
+  //       let transition = transitionArc(wedge, label);
+  //       return transition?.interpolator;
+  //     });
+  // }
 
   // function 3/6 - called above
-  function transitionArc(wedge, label) {
-    label ??= Object.entries(wedges).find(([label, w]) => w === wedge)[0];
-    let d = pieData.find((d) => d.label === label);
-    let d_old = oldData.find((d) => d.label === label);
+  // function transitionArc(wedge, label) {
+  //   label ??= Object.entries(wedges).find(([label, w]) => w === wedge)[0];
+  //   let d = pieData.find((d) => d.label === label);
+  //   let d_old = oldData.find((d) => d.label === label);
 
-    if (sameArc(d_old, d)) {
-      return null;
-    }
+  //   if (sameArc(d_old, d)) {
+  //     return null;
+  //   }
 
-    let from = d_old ? { ...d_old } : getEmptyArc(label, oldData);
-    let to = d ? { ...d } : getEmptyArc(label, pieData);
-    let angleInterpolator = d3.interpolate(from, to);
+  //   let from = d_old ? { ...d_old } : getEmptyArc(label, oldData);
+  //   let to = d ? { ...d } : getEmptyArc(label, pieData);
+  //   let angleInterpolator = d3.interpolate(from, to);
 
-    let interpolator = (t) => `path("${arcGenerator(angleInterpolator(t))}")`;
+  //   let interpolator = (t) => `path("${arcGenerator(angleInterpolator(t))}")`;
 
-    let type = d ? (d_old ? "update" : "in") : "out";
+  //   let type = d ? (d_old ? "update" : "in") : "out";
 
-    return { d, d_old, from, to, interpolator, type };
-  }
+  //   return { d, d_old, from, to, interpolator, type };
+  // }
 
   // function 4/6 - used as a transition directive in path for svg below
   // possible source of error............
-  function arc(wedge) {
-    return {
-      duration: transitionDuration,
-      easing: d3.easeCubic,
-      css: (t, u) => {
-        let transition = transitionArc(wedge);
-        if (!transition) return "";
+  // function arc(wedge) {
+  //   return {
+  //     duration: transitionDuration,
+  //     easing: d3.easeCubic,
+  //     css: (t, u) => {
+  //       let transition = transitionArc(wedge);
+  //       if (!transition) return "";
 
-        let { d, d_old, from, to, type } = transition;
-        let arcString = type === "update" ? d : type === "in" ? to : from;
-        return `path("${arcGenerator(arcString)}")`;
-      },
-    };
-  }
+  //       let { d, d_old, from, to, type } = transition;
+  //       let arcString = type === "update" ? d : type === "in" ? to : from;
+  //       return `path("${arcGenerator(arcString)}")`;
+  //     },
+  //   };
+  // }
 
-  // function 5/6 - given
-  function getEmptyArc(label, data = pieData) {
-    let labels = d3.sort(new Set([...oldData, ...pieData].map((d) => d.label)));
-    let labelIndex = labels.indexOf(label);
-    let sibling;
-    for (let i = labelIndex - 1; i >= 0; i--) {
-      sibling = data.find((d) => d.label === labels[i]);
-      if (sibling) {
-        break;
-      }
-    }
+  // // function 5/6 - given
+  // function getEmptyArc(label, data = pieData) {
+  //   let labels = d3.sort(new Set([...oldData, ...pieData].map((d) => d.label)));
+  //   let labelIndex = labels.indexOf(label);
+  //   let sibling;
+  //   for (let i = labelIndex - 1; i >= 0; i--) {
+  //     sibling = data.find((d) => d.label === labels[i]);
+  //     if (sibling) {
+  //       break;
+  //     }
+  //   }
 
-    let angle = sibling?.endAngle ?? 0;
-    return { startAngle: angle, endAngle: angle };
-  }
+  //   let angle = sibling?.endAngle ?? 0;
+  //   return { startAngle: angle, endAngle: angle };
+  // }
 
   // function 6/6 - defined previously
-  function toggleWedge(label, event) {
-    if (!event.key || event.key === "Enter") {
-      selectedIndex = selectedIndex === label ? -1 : label;
-    }
-  }
+  // function toggleWedge(label, event) {
+  //   if (!event.key || event.key === "Enter") {
+  //     selectedIndex = selectedIndex === label ? -1 : label;
+  //   }
+  // }
 </script>
 
 <div class="container">
   <svg viewBox="-50 -50 100 100">
-    {#each pieData as d, index (d.label)}
+    {#each pieData as d, index}
+      <!-- {#each pieData as d, index (d.label)} 
+     on:click={(e) => toggleWedge(d.label, e)}
+        on:keyup={(e) => toggleWedge(d.label, e)} bind:this={wedges[d.label]}
+        style="transition: d {transitionDuration}ms;"
+        transition:arc-->
       <path
         d={d.arc}
         fill={colors(d.id ?? d.label)}
         fill-opacity="0.75"
-        class:selected={selectedIndex === d.label}
-        on:click={(e) => toggleWedge(d.label, e)}
-        on:keyup={(e) => toggleWedge(d.label, e)}
+        class:selected={selectedIndex === index}
         tabindex="0"
         role="button"
         aria-label="Select Wedge"
-        bind:this={wedges[d.label]}
-        style="transition: d {transitionDuration}ms;"
-        transition:arc
+        on:click={(e) => (selectedIndex = selectedIndex === index ? -1 : index)}
       />
     {/each}
   </svg>
 
   <ul class="legend">
-    {#each pieData as d, index (d.label)}
-      <li style="--color: {colors(d.label)}">
+    {#each pieData as d, index}
+      <!-- {#each pieData as d, index (d.label)} -->
+      <li
+        style="--color: {colors(index)}"
+        class:selected={selectedIndex === index}
+      >
         <span class="swatch"></span>
         {d.label} <em>({d.value})</em>
       </li>
